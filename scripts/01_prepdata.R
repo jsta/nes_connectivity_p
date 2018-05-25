8bdbb797d032cca53a8b1ec421ede031f126faa6
jsta <stachel2@msu.edu>
2018-05-25 12:33:49 -0400

    speed up processing by not repulling data

--- scripts/01_prepdata.R
+++ scripts/01_prepdata.R
@@ -12,15 +12,18 @@ if(file.exists("../data")){
   pre_path <- "data"
 }
 
-nes_iws <- prep_full_nes(file.path(pre_path, "nes_x_lagos-ne.csv"), 
+if(!all(sapply(c("nes_iws", "nes_nws", "nes_rf_iws", "nes_rf_nws"), exists))){
+  
+  nes_iws <- prep_full_nes(file.path(pre_path, "nes_x_lagos-ne.csv"), 
                          file.path(pre_path, "connectivity_metrics_iws.csv"))
 
-nes_nws <- prep_full_nes(file.path(pre_path, "nes_x_lagos-ne.csv"), 
+  nes_nws <- prep_full_nes(file.path(pre_path, "nes_x_lagos-ne.csv"), 
                          file.path(pre_path, "connectivity_metrics_nws.csv"))
 
-# RandomForest data
-nes_rf_iws <- make_rf_dt(nes_iws)$nes_rf
-# make_rf_dt(nes_iws)$nes_rf_name_key
+  # RandomForest data
+  nes_rf_iws <- make_rf_dt(nes_iws)$nes_rf
+  # make_rf_dt(nes_iws)$nes_rf_name_key
 
-nes_rf_nws <- make_rf_dt(nes_nws)$nes_rf
-# make_rf_dt(nes_nws)$nes_rf_name_key
+  nes_rf_nws <- make_rf_dt(nes_nws)$nes_rf
+  # make_rf_dt(nes_nws)$nes_rf_name_key
+}
