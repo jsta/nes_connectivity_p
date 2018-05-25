fa602e80b55b3f1a40cfff20bb4aa4a3627f839a
jsta <stachel2@msu.edu>
2018-05-25 13:20:30 -0400

    revert check

--- scripts/01_prepdata.R
+++ scripts/01_prepdata.R
@@ -12,7 +12,7 @@ if(file.exists("../data")){
   pre_path <- "data"
 }
 
-if(!all(sapply(c("nes_iws", "nes_nws", "nes_rf_iws", "nes_rf_nws"), exists))){
+# if(!all(sapply(c("nes_iws", "nes_nws", "nes_rf_iws", "nes_rf_nws"), exists))){
   
   nes_iws <- prep_full_nes(file.path(pre_path, "nes_x_lagos-ne.csv"), 
                          file.path(pre_path, "connectivity_metrics_iws.csv"))
@@ -26,4 +26,4 @@ if(!all(sapply(c("nes_iws", "nes_nws", "nes_rf_iws", "nes_rf_nws"), exists))){
 
   nes_rf_nws <- make_rf_dt(nes_nws)$nes_rf
   # make_rf_dt(nes_nws)$nes_rf_name_key
-}
+# }
