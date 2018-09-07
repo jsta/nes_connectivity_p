ff6845eb55932a222394a843b9f973fe9b970192
jsta <stachel2@msu.edu>
2018-09-07 12:01:06 -0400

    calculate effect sizes for all metrics

--- scripts/05_result-stats.R
+++ scripts/05_result-stats.R
@@ -7,52 +7,76 @@ library(dplyr)
 library(magrittr)
 
 dt           <- read.csv("../figures/table_1.csv")
-stan_results <- readRDS("../data/nws/ll_vollenweider.rds")
 
-median_x     <- median(
-  brms::posterior_samples(stan_results, pars = "b_x_Intercept")$b_x_Intercept)
-# lower_x     <- as.numeric(quantile(
-#   brms::posterior_samples(stan_results, pars = "b_x_Intercept")$b_x_Intercept, 
-#   0.025))
-# higher_x     <- as.numeric(quantile(
-#   brms::posterior_samples(stan_results, pars = "b_x_Intercept")$b_x_Intercept, 
-#   0.975))
-
-median_tau   <- stan_results$data %>% 
-  summarize(median_tau = median(retention_time_yr))
-median_tau <- median_tau$median_tau
-
-quant_tau <- as.numeric(quantile(
-  stan_results$data$retention_time_yr, c(0.025, 0.5, 0.975)))
-
-k <- apply(
-  brms::posterior_samples(stan_results, pars = c("b_k_part1", "b_k_part2")), 
-  2, quantile)
-
-p_retention <- function(k, tau, x){
-  round((1 - (1/(1 + k * (tau^x)))), 3) 
+p_retention_by_grp <- function(rds_path){
+  stan_results <- readRDS(rds_path)
+  
+  median_x     <- median(
+    brms::posterior_samples(stan_results, pars = "b_x_Intercept")$b_x_Intercept)
+  
+  median_tau   <- stan_results$data %>% 
+    summarize(median_tau = median(retention_time_yr))
+  median_tau <- median_tau$median_tau
+  
+  quant_tau <- as.numeric(quantile(
+    stan_results$data$retention_time_yr, c(0.025, 0.5, 0.975)))
+  
+  k <- apply(
+    brms::posterior_samples(stan_results, pars = c("b_k_part1", "b_k_part2")), 
+    2, quantile)
+  
+  p_retention <- function(k, tau, x){
+    round((1 - (1/(1 + k * (tau^x)))), 3) 
+  }
+  
+  lower_k_grid <- setNames(expand.grid(k[2:4, 1], quant_tau), c("k", "tau"))
+  upper_k_grid <- setNames(expand.grid(k[2:4, 2], quant_tau), c("k", "tau"))
+  
+  lower_k_grid$p_retention <- apply(lower_k_grid, 1, 
+                                    function(x) p_retention(x[1], x[2], 
+                                                            x = median_x))
+  upper_k_grid$p_retention <- apply(upper_k_grid, 1, 
+                                    function(x) p_retention(x[1], x[2], 
+                                                            x = median_x))
+  
+  effect_sizes <- data.frame(matrix(
+    lower_k_grid$p_retention - upper_k_grid$p_retention, 
+    nrow = 3, ncol = 3))
+  row.names(effect_sizes) <- c("low", "medium", "high")
+  names(effect_sizes) <- c("low", "medium", "high")
+  
+  # print(effect_sizes)
+  # knitr::kable(effect_sizes, format = "html") %>%
+  #   kableExtra::add_header_above(c("", "", "tau", ""))
+  
+  data.frame(
+    effect_sizes = c(effect_sizes$medium[3], effect_sizes$medium[1]),
+    p_retention = c(upper_k_grid$p_retention[5], lower_k_grid$p_retention[5]), 
+    stringsAsFactors = FALSE)
 }
 
-lower_k_grid <- expand.grid(k[2:4, 1], quant_tau)
-upper_k_grid <- expand.grid(k[2:4, 2], quant_tau)
+rds_paths <- list("../data/lc_vollenweider.rds", #1
+                  "../data/md_vollenweider.rds", #2
+                  "../data/iws/cd_vollenweider.rds", #3
+                  "../data/iws/sd_vollenweider.rds", #4
+                  "../data/iws/bf_vollenweider.rds", #5
+                  "../data/iws/ll_vollenweider.rds",
+                  "../data/iws/la_vollenweider.rds",
+                  "../data/iws/sr_vollenweider.rds", 
+                  "../data/nws/cd_vollenweider.rds", 
+                  "../data/nws/ll_vollenweider.rds", 
+                  "../data/nws/la_vollenweider.rds", #11
+                  "../data/nws/bf_vollenweider.rds", 
+                  "../data/nws/sd_vollenweider.rds", 
+                  "../data/nws/sr_vollenweider.rds")
 
-lower_k_grid$p_retention <- apply(lower_k_grid, 1, 
-                                  function(x) p_retention(x[1], x[2], x = median_x))
-upper_k_grid$p_retention <- apply(upper_k_grid, 1, 
-                                  function(x) p_retention(x[1], x[2], x = median_x))
-
-effect_sizes <- data.frame(matrix(
-  lower_k_grid$p_retention - upper_k_grid$p_retention, 
-       nrow = 3, ncol = 3))
-row.names(effect_sizes) <- c("low", "medium", "high")
-names(effect_sizes) <- c("low", "medium", "high")
-
-knitr::kable(effect_sizes, format = "html") %>%
-  kableExtra::add_header_above(c("", "", "tau", ""))
+res <- lapply(rds_paths, p_retention_by_grp)
+names(res) <- make.names(rds_paths)
+lapply(res, function(x) any(abs(x$effect_sizes) < 0.009))
 
 paste0("At median water residence time, k, and tau lakes with shorter and 
       longer link lengths respectively had a P retention of ", 
-       upper_k_grid$p_retention[5], " and ", lower_k_grid$p_retention[5], ".")
+       res$...data.nws.ll_vollenweider.rds$p_retention[1], " and ", res$...data.nws.ll_vollenweider.rds$p_retention[2], ".")
 
 # ---- number_of_study_lakes ----
 
